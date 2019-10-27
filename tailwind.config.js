85076960985c59b5ec96436e634dda1c80739a7a
Tripp Shelnutt <shelnutt77@gmail.com>
2019-10-27 16:41:09 -0400

    new tailwind styles and bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    theme: {},
+    theme: {
+      opacity: {
+        '10': '.1',
+      },
+      zIndex: {
+        '-1': -1,
+      },
+      extend: {
+        colors: {
+          'linkedin': '#0077B5',
+          'angellist': '#000000',
+          'twitter': '#1da1f2',
+          'github': '#6e5494',
+          'aboutme': '#00a98f',
+          'facebook': '#3b5998',
+        }
+      }
+    },
     variants: {},
     plugins: [],
   }
