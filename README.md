95232834a8da684af045930fe6fbc00c13e16e7f
Tripp Shelnutt <trippshelnutt@users.noreply.github.com>
2022-09-03 16:35:34 -0400

    Migrated to Eleventy (#10)
    * Migrated to Eleventy
    
    * fixup! Migrated to Eleventy

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # trippshelnutt.com
-Project for [my landing site](https://trippshelnutt.com) - built with Jekyll, TailwindCSS, and hosted on Netlify.
+Project for [my landing site](https://trippshelnutt.com) - built with Eleventy, TailwindCSS, and hosted on Netlify.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    theme: {
-      opacity: {
-        '10': '.1',
-      },
-      zIndex: {
-        '-1': -1,
-      },
-      extend: {
-        colors: {
-          'linkedin': '#0077B5',
-          'twitter': '#1da1f2',
-          'github': '#6e5494',
-        }
-      }
+  content: ["./index.html"],
+  theme: {
+    opacity: {
+      10: ".1",
     },
-    variants: {},
-    plugins: [],
-  }
+    zIndex: {
+      "-1": -1,
+    },
+    extend: {
+      colors: {
+        linkedin: "#0077B5",
+        twitter: "#1da1f2",
+        github: "#6e5494",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
